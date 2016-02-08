750d54b885d0dcc18f891155d563284c5e74e9ef
tcasey <trevort.casey@gmail.com>
2016-02-08 15:28:28 -0700

    working on some OOP

--- practice.js
+++ practice.js
@@ -3,7 +3,12 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+var me = {
+  name: 'Trevor',
+  age: 24,
+};
+
+alert(me.name);
 
 
 
@@ -15,17 +20,26 @@
 
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+  var favoriteThings = {
+    band: 'Cold Play',
+    food: 'Everything Italian',
+    person: 'Meg',
+    book: 'The Martian',
+    movie: "Ferris Bueller's Day Off",
+    holiday: 'Thanksgiving'
+  };
 
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
+  favoriteThings.car = 'Hilux';
+  favoriteThings.brand = 'Toyota';
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
-  //Code here
+  favoriteThings.food = 'Lettuce';
+  favoriteThings.book = '50 Shades of Gray';
 
 
 
@@ -42,17 +56,20 @@ Set the value of that key to 'chapstick'.
 Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
-  //Code here
+var backPack = new Object();
+var item = 'firstPocket';
+backPack.item = 'firstPocket';
+backPack.firstPocket = 'chapstick';
 
 //After you do the above, alert your entire backPack object.
 
-  //Code here
+alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
-  //Code here
+consol.log(backPack);
 
 
 
@@ -64,12 +81,21 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 //Create an 'alsoMe' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
 
-  //Code Here
+  var alsoMe ={
+    name: 'Trevor',
+    age: 24,
+    height: 'pretty short',
+    gender: 'male',
+    married: 'Ummm yeah',
+    eyeColor: 'blue',
+    hairColor: 'brown',
+  };
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
-  //Code Here
-
+  // for (var i in alsoMe) {
+  //   alert(alsoMe[i]);
+  // }
 
 
 
@@ -80,11 +106,19 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 //Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
 
-  //Code Here
+  var album = {
+    waterfall: 3,
+    sunshine: 2,
+    skyfall: 4,
+    birds: 5,
+    dust: 7
+  };
 
 //Now, loop through your album object alerting every song title individually.
 
-  //Code Here
+  for (var  in album) {
+    alert('');
+  }
 
 
 
@@ -196,9 +230,9 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 // Create a function called MakeCard which takes in all the data it needs to make a Credit Card object and returns that object so that whenever you invoke MakeCard, you get a brand new credit card.
 
   //Code Here
-  
-  
-  
+
+
+
 //NEXT PROBLEM
 
 
@@ -209,5 +243,3 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 */
 
   //Code Here
-
-
