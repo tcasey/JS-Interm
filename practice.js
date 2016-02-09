b197ab856af0528f8c72a79a03aeba34ab8e5395
tcasey <trevort.casey@gmail.com>
2016-02-09 10:20:31 -0700

    super late push

--- practice.js
+++ practice.js
@@ -69,7 +69,7 @@ alert(backPack);
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
-consol.log(backPack);
+console.log(backPack);
 
 
 
@@ -93,9 +93,9 @@ consol.log(backPack);
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
-  // for (var i in alsoMe) {
-  //   alert(alsoMe[i]);
-  // }
+  for (var i in alsoMe) {
+    alert(alsoMe[i]);
+  }
 
 
 
@@ -116,8 +116,8 @@ consol.log(backPack);
 
 //Now, loop through your album object alerting every song title individually.
 
-  for (var  in album) {
-    alert('');
+  for (var key in album) {
+    alert(album[key]);
   }
 
 
@@ -130,11 +130,20 @@ consol.log(backPack);
 
 //Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
 
-  //Code Here
-
+var states = {
+  utah: '6000',
+  cali: '500000',
+  oregon: '900000',
+  newYork: '20000',
+  florida: '5000'
+}
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
-  //Code Here
+for (var p in states) {
+ if(states[p] > 30000) {
+   alert(p);
+ }
+}
 
 
 
@@ -155,11 +164,20 @@ var user1 = {
 /*Above you're given a user object. Loop through the user object checking to make sure
 that each value is truthy. If it's not truthy, remove it from the object. */
 
-  //Code Here
+for (var p in user1) {
+  if (user1[p]) {
+
+  }
+  else {
+    delete user1[p];
+  }
+}
 
 //Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
-  //Code Here
+  user1.name = 'Trevor';
+  user1.username = 'tcasey';
+
 
 
 
@@ -183,13 +201,12 @@ var user2 = {
 //Let's say I, the user, decided to change my name and email address to the following
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
-  //Code Here
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 //Now call the sayName method that's on the user object which will alert the users email
 
-  //Code Here
-
-
+user2.sayName();
 
 
 //NEXT PROBLEM
@@ -199,17 +216,24 @@ var user2 = {
 
 //Create an empty object called methodCollection.
 
-  //Code Here
+var methodCollection = {};
 
 /*Now add two methods (functions that are properties on objects) to your methodCollection
 object. One called 'alertHello' which alerts 'hello' and another method called logHello
  which logs 'hello' to the console. */
 
-  //Code Here
+methodCollection.alertHello = function () {
+
+  alert('hello');
+}
+methodCollection.logHello = function () {
+  console.log('hello');
+}
 
 //Now call your alertHello and logHello methods.
 
-  //Code Here
+methodCollection.alertHello ();
+methodCollection.logHello ();
 
 
 
@@ -219,7 +243,15 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
 // Create a function called MakePerson which takes in name, birthday, ssn as its parameters and returns a new object with all of the information that you passed in.
 
-  //Code Here
+function MakePerson(name, birthday, ssn) {
+  var schtuff = new Object();
+  schtuff.name = name;
+  schtuff.birthday = birthday;
+  schtuff.ssn = ssn;
+
+  return schtuff;
+
+}
 
 
 
@@ -229,7 +261,16 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
 // Create a function called MakeCard which takes in all the data it needs to make a Credit Card object and returns that object so that whenever you invoke MakeCard, you get a brand new credit card.
 
-  //Code Here
+function MakeCard() {
+  var master = {
+    name: '',
+    expDate: '',
+    cardNumber: ''
+
+  };
+  return master;
+}
+
 
 
 
@@ -242,4 +283,9 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
    Have bindCard merge the two parameters together into a new object which contains all the properties from the person as well as the creditcard.
 */
 
-  //Code Here
+function bindCard(MakePerson, MakeCard) {
+  var newCustomer ={
+
+  }
+  return newCustomer;
+}
